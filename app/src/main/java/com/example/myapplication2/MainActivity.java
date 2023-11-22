7d4a9fb084e09cf1a6b65e2ae8b68e30d8ab70bf
Shani Kumar2 <Sk00788460@techmahindra.com>
2023-11-22 17:21:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+
+    }
+}
