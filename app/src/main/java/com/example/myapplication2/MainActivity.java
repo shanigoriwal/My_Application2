b8510c92b4078d5f168e7b2f5eff4de2dda0d9cb
Shani Kumar2 <SK00788460@techmahindra.com>
2023-12-01 19:09:37 +0530

    Handle Click listner

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.myapplication2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        //This is Testing of Shani
+            }
+        });
+         name= findViewById(R.id.button);
+
     }
 }
